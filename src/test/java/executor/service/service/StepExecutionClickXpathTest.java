6574937100605f6de4aa0e2cfb85683a3d132ab7
AlexandrTokarenko <72304367+AlexandrTokarenko@users.noreply.github.com>
2023-03-28 19:47:33 +0300

    Create Test service StepExecution ClickXpath (#34)
    * Create Test service StepExecution ClickXpath
    
    * Make all methods public
    
    * Add .close() method call
    
    Also we can use AutoCloseable with try-with-resources

---
+++ src/test/java/executor/service/service/StepExecutionClickXpathTest.java
@@ -0,0 +1,80 @@
+package executor.service.service;
+
+import executor.service.model.StepDTO;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StepExecutionClickXpathTest {
+
+    private final String clickXpath = "clickXpath";
+    @Mock
+    private WebDriver webDriver;
+    private AutoCloseable autoCloseable;
+    @Mock
+    private WebElement webElement;
+    @InjectMocks
+    private StepExecutionClickXpath stepExecutionClickXpath;
+    private StepDTO stepDTO;
+
+    @BeforeEach
+    public void setUp() {
+        autoCloseable = MockitoAnnotations.openMocks(this);
+        stepDTO = new StepDTO(clickXpath,"//form/div[1]//div[2]/input");
+   }
+
+    @AfterEach
+    void tearDown() throws Exception {
+        autoCloseable.close();
+    }
+
+    @Test
+    public void testInitialize() {
+
+        assertNotNull(stepExecutionClickXpath);
+    }
+
+    @Test
+    public void testGetStepAction() {
+
+        String actual = stepExecutionClickXpath.getStepAction();
+        assertEquals(clickXpath, actual);
+    }
+
+    @Test
+    public void testFindElement() {
+
+        Mockito.when(webDriver.findElement(By.xpath(stepDTO.getValue()))).thenReturn(webElement);
+        stepExecutionClickXpath.step(webDriver, stepDTO);
+        Mockito.verify(webDriver).findElement(By.xpath(stepDTO.getValue()));
+    }
+
+
+    @Test
+    public void testWebElementClick() {
+
+        Mockito.when(webDriver.findElement(By.xpath(stepDTO.getValue()))).thenReturn(webElement);
+        stepExecutionClickXpath.step(webDriver, stepDTO);
+        Mockito.verify(webElement).click();
+    }
+
+    @Test
+    public void testWrongSelector() {
+
+        String value = "Something";
+        StepDTO wrongStep = new StepDTO(clickXpath, value);
+        Mockito.when(webDriver.findElement(By.xpath(value))).thenThrow(NoSuchElementException.class);
+
+        assertThrows(NoSuchElementException.class, () -> stepExecutionClickXpath.step(webDriver, wrongStep));
+    }
+}
