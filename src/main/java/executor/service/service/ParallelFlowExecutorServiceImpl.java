063bfa9395f1d646c038429ec3774bdc3937bd8a
Serhii Matvienko <111274705+Serhx4@users.noreply.github.com>
2023-04-13 13:26:08 +0300

    Create service facade ParallelFlowExecutorService (#48)

---
+++ src/main/java/executor/service/service/ParallelFlowExecutorServiceImpl.java
@@ -0,0 +1,47 @@
+package executor.service.service;
+
+import executor.service.model.ProxyConfigHolderDTO;
+import executor.service.model.ScenarioDTO;
+import executor.service.model.ThreadPoolConfigDTO;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ParallelFlowExecutorServiceImpl implements ParallelFlowExecutorService {
+
+    private final ScenarioSourceListener scenarioSourceListener;
+    private final ProxySourcesClientImpl proxySourcesClient;
+    private final ExecutionService executionService;
+    private final ThreadPoolExecutor threadPoolExecutor;
+
+    public ParallelFlowExecutorServiceImpl(ScenarioSourceListener scenarioSourceListener,
+                                           ProxySourcesClientImpl proxySourcesClient,
+                                           ExecutionService executionService,
+                                           ThreadPoolConfigDTO threadPoolConfig,
+                                           ThreadPoolExecutor threadPoolExecutor) {
+        this.scenarioSourceListener = scenarioSourceListener;
+        this.proxySourcesClient = proxySourcesClient;
+        this.executionService = executionService;
+        this.threadPoolExecutor = threadPoolExecutor;
+        threadPoolExecutor.setCorePoolSize(threadPoolConfig.getCorePoolSize());
+        threadPoolExecutor.setKeepAliveTime(threadPoolConfig.getKeepAliveTime(), TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void runParallel() {
+        while (true) {
+            ScenarioDTO scenario = scenarioSourceListener.getScenario();
+            if (scenario == null) {
+                break;
+            }
+            executeScenarioInParallel(scenario);
+        }
+        threadPoolExecutor.shutdown();
+    }
+
+    private void executeScenarioInParallel(ScenarioDTO scenario) {
+        ProxyConfigHolderDTO proxy = proxySourcesClient.getProxy();
+        threadPoolExecutor.execute(() -> executionService.execute(scenario, proxy));
+    }
+
+}
